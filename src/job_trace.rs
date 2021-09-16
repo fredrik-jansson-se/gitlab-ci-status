5f1b31c4615ea9fc36216048c368875345dbf48e
Fredrik Jansson <fj@avassa.io>
2021-09-16 08:31:34 +0200

    Stream logs

--- src/job_trace.rs
+++ src/job_trace.rs
@@ -1,7 +1,9 @@
+use futures_util::StreamExt;
 use tui::{
     backend::Backend,
     layout::Constraint,
     style::{Modifier, Style},
+    text::{Span, Spans},
     widgets::{Block, Cell, Paragraph, Table, TableState},
     Terminal,
 };
@@ -15,47 +17,41 @@ pub(crate) async fn run<B: Backend>(
     job_id: u64,
 ) -> anyhow::Result<()> {
     tracing::info!("run");
-    let mut last_update = chrono::Local::now() - chrono::Duration::seconds(100);
-    let mut logs = String::new();
+    let uri = format!(
+        "{}/projects/{}/jobs/{}/trace",
+        crate::BASE_URL,
+        project_id,
+        job_id
+    );
+
+    let mut logs = Vec::new();
+    let mut logstream = client.get(uri).send().await?.bytes_stream();
+    let mut log_strings = Vec::new();
+    terminal.clear()?;
     loop {
         match key_rx.recv().await {
             None => return Ok(()),
             Some(event) => match event {
                 crate::events::Event::Tick => (),
                 crate::events::Event::Key(k) => match k {
-                    termion::event::Key::Esc => return Ok(()),
+                    termion::event::Key::Esc => {
+                        terminal.clear()?;
+                        return Ok(());
+                    }
                     _ => (),
                 },
             },
         }
-        if (chrono::Local::now() - last_update) > chrono::Duration::seconds(30) {
-            last_update = chrono::Local::now();
-            let uri = format!(
-                "{}/projects/{}/jobs/{}/trace",
-                crate::BASE_URL,
-                project_id,
-                job_id
-            );
-
-            logs = client.get(uri).send().await?.text().await?;
-            // tracing::error!(?jobs);
+        if let Some(Ok(buf)) = logstream.next().await {
+            logs.push(buf);
+            let log_string = String::from_utf8(logs.iter().flatten().cloned().collect())?;
+            // tracing::info!("No lines: {}", log_spans.len());
+            let mut new_log_strings = log_string.lines().map(|s| s.to_string()).collect();
+            terminal.clear()?;
+            for l in &new_log_strings {
+                print!("{}\r\n", l);
+            }
+            log_strings.append(&mut new_log_strings);
         }
-
-        terminal.draw(|f| {
-            let size = f.size();
-            // tracing::info!(?size);
-            let log_lines = logs.lines().count() as u16;
-            let scroll_offset = if log_lines > size.height {
-                log_lines - size.height
-                // 10
-            } else {
-                0
-            };
-            let paragraph = Paragraph::new(logs.clone()).scroll((scroll_offset, 0));
-            f.render_widget(paragraph, f.size());
-        });
-        //GET /projects/:id/jobs/:job_id/trace
     }
-    // tracing::info!(?jobs_json);
-    Ok(())
 }
